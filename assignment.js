61e78b02bdaead4080bf54e2294c47ceedf47d17
Kashaf1212 <kashafmalik1212@gmail.com>
2023-06-20 19:46:55 +0500

    program

---
+++ assignment.js
@@ -0,0 +1,11 @@
+// Calculate area----->> 
+function calculateArea(length, width){
+    let area = length * width;
+    console.log("the area of rectangular is", area);
+    return area;
+
+    
+}
+calculateArea(23, 15);
+
+
